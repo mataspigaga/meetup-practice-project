05624b67c41e1f9e8189ca724e89f137444da227
Matas Pigaga <mataspigaga@gmail.com>
2021-07-11 14:40:22 -0400

    added styled meetup detail page with dummy data

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      image="https://picsum.photos/seed/firstmeetup/1500"
+      title="The First Meetup"
+      address="123 Main St, City, State, 12345"
+      description="Test description for the first meetup"
+    />
+  );
+};
+
+export default MeetupDetails;
