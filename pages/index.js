e9715d81a005ac69e4feef5632e8b9bc94617bc3
Matas Pigaga <mataspigaga@gmail.com>
2021-07-11 14:15:20 -0400

    added home page and new meetup form

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+// domain.com/
+
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image: "https://picsum.photos/seed/firstmeetup/1500",
+    address: "123 Main St, City, State, 12345",
+    description: "This is the first test description",
+  },
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image: "https://picsum.photos/seed/secondmeetup/1500",
+    address: "456 Main St, City, State, 12345",
+    description: "This is the second test description",
+  },
+];
+
+const HomePage = () => {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+};
+
+export default HomePage;
