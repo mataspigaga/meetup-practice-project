80984990758cd8f22c6ff2bb81cb362f2d687118
Matas Pigaga <mataspigaga@gmail.com>
2021-07-11 17:27:41 -0400

    added meta data and titles

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,5 +1,6 @@
 // domain.com/new-meetup
 
+import Head from "next/head";
 import { useRouter } from "next/router";
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
@@ -22,7 +23,18 @@ const NewMeetupPage = () => {
     router.push("/");
   };
 
-  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+  return (
+    <>
+      <Head>
+        <title>Add a New Meetup</title>
+        <meta
+          name="description"
+          content="Add your own meetups and create amazing networking opportunities."
+        />
+      </Head>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </>
+  );
 };
 
 export default NewMeetupPage;
