c29f2a741d6cced2fb8c016fcf416b2521f6240e
Matas Pigaga <mataspigaga@gmail.com>
2021-07-11 15:19:42 -0400

    added detail page build time data gen & logo click

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -4,7 +4,9 @@ import classes from "./MainNavigation.module.css";
 function MainNavigation() {
   return (
     <header className={classes.header}>
-      <div className={classes.logo}>React Meetups</div>
+      <div className={classes.logo}>
+        <Link href="/">React Meetups</Link>
+      </div>
       <nav>
         <ul>
           <li>
--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,41 @@ const MeetupDetails = () => {
   );
 };
 
+export const getStaticPaths = async () => {
+  return {
+    // indicates that all paths are defined when false
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+};
+
+export const getStaticProps = async (context) => {
+  // fetch data for a single meetup
+
+  const meetupId = context.params.meetupId;
+
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        image: "https://picsum.photos/seed/firstmeetup/1500",
+        title: "The First Meetup",
+        address: "123 Main St, City, State, 12345",
+        description: "Test description for the first meetup",
+      },
+    },
+  };
+};
+
 export default MeetupDetails;
