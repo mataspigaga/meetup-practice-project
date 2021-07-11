ade81715be701ee2df5d05e472bef88beaaa813b
Matas Pigaga <mataspigaga@gmail.com>
2021-07-11 16:35:52 -0400

    added storage of new meetup data in MongoDB

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,28 @@
+// api/new-meetup
+// POST /api/new-meetup
+
+import { MongoClient } from "mongodb";
+
+const handler = async (req, res) => {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    // never run client-side, hence it's in the api folder
+    const client = await MongoClient.connect(
+      "mongodb+srv://mataspigaga:Centennial1905@cluster0.wnsdo.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
+  }
+};
+
+export default handler;
